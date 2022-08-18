dcc78a533c7bda502f6c8ce2cf691f568e4fb8da
dailybgl <103235926+dailybgl@users.noreply.github.com>
2022-08-19 02:38:04 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,46 @@
+-- BOTS CONFIG
+BOTS = {}
+
+BOTS["BOTNAME1"] = {
+    world = {"worldforbot1","worldforbot1","worldforbot1"},
+    webhook = "LINK"
+}
+BOTS["BOTNAME2"] = {
+    world = {"worldforbot2","worldforbot2","worldforbot2"},
+    webhook = "LINK"
+}
+
+StartWorld = 1
+LoopWorld = true
+OwnerID = "YOUR_DISCORD_ID"
+--FARM CONFIG
+ModeSoil = false
+TrashRoot = false
+SaveSeedWorld = "world"
+SaveRootWorld = "world"
+SavePackWorld = "world"
+----------------------
+DoorIdFARM = "doorfarmid"
+SaveDoorID = "savedoorid"
+----------------------
+type_block = "fg"
+FarmableSeedID = 8641
+RootCuttingID = 8746
+----------------------
+patokanDropSeed = 5040
+patokanDropRoot = 4584
+patokanDropPack = 1422
+--FARMING CONFIG
+break_pos_x = 0
+break_pos_y = 1
+delaypnb = 150
+delayplant = 100 
+delayharvest = 100
+delayworld = 5000
+--PACK CONFIG
+pack = "world_lock_10_pack"
+price_pack = 20000
+countBuy = 10
+pack_id = {242}
+trash_list = {5036,7164,7162,5034,5040,5044,5028,5042,5032,5038,5024,5026,5030}
+--=====|| ||=====---
