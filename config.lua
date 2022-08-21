86135c3e37b58406e89057358bc708a50c164131
dailybgl <103235926+dailybgl@users.noreply.github.com>
2022-08-21 13:56:15 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,46 +1,47 @@
 -- BOTS CONFIG
 BOTS = {}
-
-BOTS["BOTNAME1"] = {
-    world = {"worldforbot1","worldforbot1","worldforbot1"},
-    webhook = "LINK"
+BOTS["JACKLAW2"] = {
+    world = {"lkrlh"},
+    webhook = "https://discord.com/api/webhooks/1006609686390374400/15kXJRicM_Rh4SDseNJVy7JJjF5Ura5jHyN7sDss8sGXJbAtPJFzRhR4sFv5yclmh-4y"
 }
-BOTS["BOTNAME2"] = {
-    world = {"worldforbot2","worldforbot2","worldforbot2"},
-    webhook = "LINK"
-}
-
 StartWorld = 1
 LoopWorld = true
-OwnerID = "YOUR_DISCORD_ID"
---FARM CONFIG
+-- WEBHOOK CONFIG
+OwnerID = "554738226800361472"
+white_list_ban = "ipvu"
+-- FARM CONFIG
 ModeSoil = false
 TrashRoot = false
-SaveSeedWorld = "world"
-SaveRootWorld = "world"
-SavePackWorld = "world"
+SaveSeedWorld = "abc"
+SaveRootWorld = "abc"
+SavePackWorld = "abc"
 ----------------------
-DoorIdFARM = "doorfarmid"
-SaveDoorID = "savedoorid"
-----------------------
-type_block = "fg"
-FarmableSeedID = 8641
-RootCuttingID = 8746
+DoorIdFARM = "doorini"
+SaveDoorID = "abc"
 ----------------------
 patokanDropSeed = 5040
-patokanDropRoot = 4584
-patokanDropPack = 1422
---FARMING CONFIG
+patokanDropRoot = 8640
+patokanDropPack = 3004
+----------------------
+type_block = "fg"
+FarmableSeedID = 4585
+RootCuttingID = 8746
+-- FARMING CONFIG
 break_pos_x = 0
 break_pos_y = 1
 delaypnb = 150
-delayplant = 100 
+delayplant = 100
 delayharvest = 100
 delayworld = 5000
---PACK CONFIG
+-- PACK CONFIG
 pack = "world_lock_10_pack"
 price_pack = 20000
-countBuy = 10
-pack_id = {242}
-trash_list = {5036,7164,7162,5034,5040,5044,5028,5042,5032,5038,5024,5026,5030}
---=====|| ||=====---
+countBuy = 20
+white_list = {[FarmableSeedID] = true, 
+              [FarmableSeedID-1] = true, 
+              [RootCuttingID] = true, 
+              [18] = true, 
+              [6336] = true, 
+              [32] = true, 
+              [98] = true
+            }
