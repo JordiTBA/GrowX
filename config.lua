bacfd7e28d48d6e9529c7af1e05954c3e1164f7e
dailybgl <103235926+dailybgl@users.noreply.github.com>
2022-08-23 08:18:38 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,47 @@
+-- BOTS CONFIG
+BOTS = {}
+BOTS["JACKLAW3"] = {
+    world = {"tnbfp"},
+    webhook = "https://discord.com/api/webhooks/1010219341045842071/iPpS1VRz_JJFpcGllxfDd82aPven_XX1-WcjE4g7CEQRR0uKJ-3AdbrwAS8f1nSu0vfR"
+}
+StartWorld = 1
+LoopWorld = true
+-- WEBHOOK CONFIG
+OwnerID = "554738226800361472"
+white_list_ban = "IPVU"
+-- FARM CONFIG
+ModeSoil = false
+TrashRoot = false
+SaveSeedWorld = "testsave100"
+SaveRootWorld = "abc"
+SavePackWorld = "testsave200"
+----------------------
+DoorIdFARM = "memeklu"
+SaveDoorID = "321"
+----------------------
+patokanDropSeed = 4584
+patokanDropRoot = 8640
+patokanDropPack = 880
+----------------------
+type_block = "fg"
+FarmableSeedID = 4585
+RootCuttingID = 8746
+-- FARMING CONFIG
+break_pos_x = 0
+break_pos_y = 1
+delaypnb = 150
+delayplant = 100
+delayharvest = 100
+delayworld = 5000
+-- PACK CONFIG
+pack = "world_lock_10_pack"
+price_pack = 20000
+countBuy = 1
+white_list = {[FarmableSeedID] = true, 
+              [FarmableSeedID-1] = true, 
+              [RootCuttingID] = true, 
+              [18] = true, 
+              [6336] = true, 
+              [32] = true, 
+              [98] = true
+            }
