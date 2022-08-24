f7cc486da1343d06a4713f639e6ef3b1011b4478
dailybgl <103235926+dailybgl@users.noreply.github.com>
2022-08-24 18:28:51 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,49 +1,51 @@
+-- // BOT CONFIG // --
 BOTS = {}
-BOTS["JACKSIMP33"] = {
-    world = {"LKRLH"},
-    webhook = "https://discord.com/api/webhooks/999276077342015519/IdsbYLv7Ebpgt7ZqDHiartIBRlvm-FuPrpQ0NM7l6wttonvG0Hakfw40r9egIYBKlp7q"
+BOTS["YOUR BOTNAME1"] = {
+    world = {"WORLD1","WORLD2","WORLD3"},
+    webhook = "YOUR_WEBHOOK_LINK"
 }   
 StartWorld = 1
 LoopWorld = true
 
 -- WEBHOOK CONFIG
-OwnerID = "554738226800361472"
-white_list_ban = "IPVU"
+OwnerID = "YOU_DISCORD_ID"
+white_list_ban = "NAME_ACCOUNT_TO_WHITELIST_BAN"
 
 -- FARM CONFIG
-ModeSoil = false
-TrashRoot = false
-SaveSeedWorld = "testsave100"
-SaveRootWorld = "abc"
-SavePackWorld = "testsave200"
+ModeSoil = false -- not support yet
+TrashRoot = false -- not support yet
+SaveSeedWorld = "YOUR_WORLD_SAVE_SEED"
+SaveRootWorld = "NOT_SUPPORT_YET"
+SavePackWorld = "YOUR_WORLD_SAVE_SEED"
 
 ----------------------
-DoorIdFARM = "daily"
-SaveDoorID = "321"
+DoorIdFARM = "YOUR_DOOR_ID_FARM"
+SaveDoorID = "YOUR_DOOR_ID_SAVE"
 
 ----------------------
-patokanDropSeed = 4584
-patokanDropRoot = 8640
+patokanDropSeed = 4584 
+patokanDropRoot = 8640 
 patokanDropPack = 880
 
 ----------------------
-type_block = "fg"
-FarmableSeedID = 4585
-RootCuttingID = 8746
+type_block = "fg" -- type block to break fg/bg (if wallpaper use bg)
+FarmableSeedID = 4585 -- your farmable seed id
+RootCuttingID = 8746 -- not support yet
 
 -- FARMING CONFIG
-break_pos_x = 0
-break_pos_y = 1
-delaypnb = 150
-delayplant = 100
-delayharvest = 100
-delayworld = 5000
+break_pos_x = 0 -- position x when bot will break
+break_pos_y = 1 -- position y when bot will break
+delaypnb = 150 -- delay put and break (if you using low rdp use high delay)
+delayplant = 100 -- delay plant
+delayharvest = 100 -- delay harvest
+delayworld = 5000 -- delay to warp world
 
 -- PACK CONFIG
-pack = "world_lock_10_pack"
-price_pack = 20000
-countBuy = 1
-white_list = {[FarmableSeedID] = true, 
+pack = "world_lock_10_pack" -- your debug pack name
+price_pack = 20000 -- your pack price
+countBuy = 1 -- how many your bot will buy
+white_list = {
+              [FarmableSeedID] = true, 
               [FarmableSeedID-1] = true, 
               [RootCuttingID] = true, 
               [18] = true, 
@@ -51,3 +53,4 @@ white_list = {[FarmableSeedID] = true,
               [32] = true, 
               [98] = true
             }
+-- // END CONFIG // --
